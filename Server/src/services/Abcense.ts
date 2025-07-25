1bdf98112a8cfe48bb16812d4e7b980a54c8adcc
Yasser <drbalyasser@gmail.com>
2025-07-25 12:20:35 +0100

    hadi mli7A

--- Server/src/services/Abcense.ts
+++ Server/src/services/Abcense.ts
@@ -10,18 +10,18 @@ interface Iabcenses {
 
 export const RegistnewAbcense =async ({identifaite,idMat,Date,idStud,cause}:Iabcenses)=>{
 if(!identifaite) return {StatusCode:402,data:"identifiante not provider"}
-if(!idMat || !Date || !idStud || !cause) return {StatusCode:402,data:"you`ve insert all informations"}
-    console.log(idMat)
+console.log(idMat)
+if(!idMat || !Date || !idStud || !cause) return {StatusCode:501,data:"you`ve insert all informations"}
+    
      const Abcense={
-        Date,cause,matieres:idMat } 
+        Date,cause,matieres:[{idMat}] } 
 const result = await Abcensesmodel.findOne({idStud})
     
  if( !result){  
-   
  const inserTnewDate = await Abcensesmodel.create({
  idStud,Abcenses:[Abcense]    
  }) 
-  if(!inserTnewDate) return {StatusCode:402,data:"failed to register info...."}
+  if(!inserTnewDate) return {StatusCode:404,data:"failed to register info...."}
   await inserTnewDate.save()
   return {StatusCode:200,data:inserTnewDate}
   }
@@ -35,7 +35,7 @@ interface IgetAbce {
 }
 export const getAbcense=async({identifaite}:IgetAbce)=>{
     if(!identifaite) return {StatusCode:402,data:"identifiante not provider"}
-const getAbc = await Abcensesmodel.find().populate("idStud").populate("Abcense.idMat")
+const getAbc = await Abcensesmodel.find().populate("idStud").populate("Abcenses.matieres.idMat")
 if(!getAbc) return {StatusCode:404,data:"conot get information of studentes "}
 return {StatusCode :200,data:getAbc}
 }
